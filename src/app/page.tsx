93fe867980edd8fd216d75bfedabb4917f3e6f6d
Khadka <120006828+khadka27@users.noreply.github.com>
2025-04-01 17:49:42 +0545

    Refactor Home component for improved readability and formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,16 @@ import { HeroSection } from "../Components/hero-section";
 import { ServicesSection } from "../Components/services-section";
 import TestimonialsSection from "../Components/testimonial-section";
 
-
 export default function Home() {
   return (
     <main className="min-h-screen">
-   
       <HeroSection />
       <FeaturesSection />
       <GoogleRankingSection />
-      <ServicesSection/>
+      <ServicesSection />
       <ClientLogos />
       <TestimonialsSection />
       <CaseStudiesSection />
-      
     </main>
-  )
+  );
 }
-
