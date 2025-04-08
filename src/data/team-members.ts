4737efc73f1cc0886a0c8cf8157882a7d4855897
Khadka <120006828+khadka27@users.noreply.github.com>
2025-04-08 14:35:41 +0545

    Refactor code structure for improved readability and maintainability

--- src/data/team-members.ts
+++ src/data/team-members.ts
@@ -19,7 +19,7 @@ export const teamMembers: TeamMember[] = [
   {
     name: "Rahul Rauniyar",
     position: "CEO of fistail info solutions",
-    avatar: rahulDai,
+    avatar: "/public/Image/rahul-rauniyar.jpeg",
     bgColor: "bg-red-100",
     slug: "rahul-runiyar",
     bio: "Maria has over 10 years of experience in digital marketing and leads our marketing team with innovative strategies that drive results for our clients.",
@@ -27,7 +27,7 @@ export const teamMembers: TeamMember[] = [
   {
     name: "Bishal Gupta",
     position: "SEO Specialist",
-    avatar: bishal,
+    avatar: "/public/Image/bishal-gupta.jpeg",
     bgColor: "bg-blue-300",
     slug: "rajan-rauniyar",
     bio: "Angela specializes in search engine optimization and has helped numerous clients achieve top rankings for competitive keywords.",
@@ -42,8 +42,8 @@ export const teamMembers: TeamMember[] = [
   },
   {
     name: "Abishek Khadka",
-    position: "Backend Developer",
-    avatar: abishek,
+    position: "Full Stack Developer",
+    avatar: "/public/Image/abishek.jpeg",
     bgColor: "bg-green-400",
     slug: "callum-bailey",
     bio: "Callum manages social media campaigns that build brand awareness and community engagement across multiple platforms.",
@@ -51,7 +51,7 @@ export const teamMembers: TeamMember[] = [
   {
     name: "Ashish Rauniyar",
     position: "Human Resources Director",
-    avatar: ashish,
+    avatar: "/public/Image/ashish.jpeg",
     bgColor: "bg-teal-300",
     slug: "daisy-howarth",
     bio: "Daisy oversees our human resources department, ensuring we attract and retain top talent while maintaining a positive company culture.",
@@ -59,7 +59,7 @@ export const teamMembers: TeamMember[] = [
   {
     name: "Pratikshya Sheersh",
     position: "Marketing Specialist",
-    avatar: pratikshya,
+    avatar: "/public/Image/partikshya.jpeg",
     bgColor: "bg-red-300",
     slug: "abby-banks",
     bio: "Abby specializes in creating and implementing marketing campaigns that deliver measurable results for our clients.",
