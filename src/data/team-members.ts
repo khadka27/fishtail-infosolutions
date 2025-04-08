285f595f91e617b3ff25a28349cceb38fcbc16b9
DiwasGurung <119999536+DiwasGurung@users.noreply.github.com>
2025-04-08 18:16:31 +0545

    image change

--- src/data/team-members.ts
+++ src/data/team-members.ts
@@ -1,9 +1,4 @@
-import avatar from "@/Images/avatar-4.png"
-import rahulDai from "@/Images/rahul-rauniyar.jpeg"
-import bishal from "@/Images/bishal-gupta.jpeg"
-import abishek from "@/Images/abishek.jpeg"
-import ashish from "@/Images/ashish.jpeg"
-import pratikshya from "@/Images/partikshya.jpeg"
+import avatar from "@/Images/abhisek.webp"
 import type { StaticImageData } from "next/image"
 
 
@@ -20,7 +15,7 @@ export const teamMembers: TeamMember[] = [
   {
     name: "Rahul Rauniyar",
     position: "CEO of fistail info solutions",
-    avatar: rahulDai,
+    avatar: avatar,
     bgColor: "bg-red-100",
     slug: "rahul-runiyar",
     bio: "Maria has over 10 years of experience in digital marketing and leads our marketing team with innovative strategies that drive results for our clients.",
@@ -28,7 +23,7 @@ export const teamMembers: TeamMember[] = [
   {
     name: "Bishal Gupta",
     position: "SEO Specialist",
-    avatar: bishal,
+    avatar:   avatar,
     bgColor: "bg-blue-300",
     slug: "rajan-rauniyar",
     bio: "Angela specializes in search engine optimization and has helped numerous clients achieve top rankings for competitive keywords.",
@@ -44,7 +39,7 @@ export const teamMembers: TeamMember[] = [
   {
     name: "Abishek Khadka",
     position: "Backend Developer",
-    avatar: abishek,
+    avatar: avatar,
     bgColor: "bg-green-400",
     slug: "callum-bailey",
     bio: "Callum manages social media campaigns that build brand awareness and community engagement across multiple platforms.",
@@ -52,7 +47,7 @@ export const teamMembers: TeamMember[] = [
   {
     name: "Ashish Rauniyar",
     position: "Human Resources Director",
-    avatar: ashish,
+    avatar:  avatar,
     bgColor: "bg-teal-300",
     slug: "daisy-howarth",
     bio: "Daisy oversees our human resources department, ensuring we attract and retain top talent while maintaining a positive company culture.",
@@ -60,7 +55,7 @@ export const teamMembers: TeamMember[] = [
   {
     name: "Pratikshya Sheersh",
     position: "Marketing Specialist",
-    avatar: pratikshya,
+    avatar: avatar,
     bgColor: "bg-red-300",
     slug: "abby-banks",
     bio: "Abby specializes in creating and implementing marketing campaigns that deliver measurable results for our clients.",
