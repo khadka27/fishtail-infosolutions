4bda1901818c32076b208de20ce831eae7a6eec2
Elvik Sharma <elviks@Elviks-MacBook-Air.local>
2025-07-04 15:33:20 +0545

    UPDATE: made changes in image

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [
-      "img.freepik.com",
-      "cdn-icons-png.freepik.com",
-      "www.freepik.com",
-      "fishtail-infosolutions.vercel.app",
-    ],
-    // Add this to ensure proper static image handling
-    dangerouslyAllowSVG: true,
-    contentDispositionType: "attachment",
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    // If you need to optimize remotely hosted images from unknown domains
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
+     images: {
+          domains: [
+               "img.freepik.com",
+               "cdn-icons-png.freepik.com",
+               "www.freepik.com",
+               "fishtail-infosolutions.vercel.app",
+               "fishtailinfosolutions.com",
+          ],
+          // Add this to ensure proper static image handling
+          dangerouslyAllowSVG: true,
+          contentDispositionType: "attachment",
+          contentSecurityPolicy:
+               "default-src 'self'; script-src 'none'; sandbox;",
+          // If you need to optimize remotely hosted images from unknown domains
+          remotePatterns: [
+               {
+                    protocol: "https",
+                    hostname: "**",
+               },
+          ],
+     },
 };
 
 module.exports = nextConfig;
